fc296485d05e21e69b4185f8d1a3e83f3574cd16
Km1zZzoU <zhovnir.ea@gmail.com>
2024-09-09 13:51:39 +0700

    first version heapsort

---
+++ Task_1_1_1/src/main/java/org/nsu/syspro/HeapSort.java
@@ -0,0 +1,68 @@
+package org.nsu.syspro;
+
+class HeapSort {
+
+    /**
+     * swap i and j in arr
+     * @param arr
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        arr[j] += arr[i];
+        arr[i] = arr[j] - arr[i];
+        arr[j] -= arr[i];
+    }
+
+    public static void sort(int arr[]) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int length = arr.length;
+
+        /**
+         * array ---> heap
+         */
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            sift(arr, length, i);
+        }
+        /**
+         * swap max to end and new array (len n-1) ---> heap
+         */
+        for (int i = length - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            sift(arr, i, 0);
+        }
+    }
+
+    /**
+     * sift the element down if thi necessary
+     * @param arr
+     * @param n
+     * @param i
+     */
+    public static void sift(int[] arr, int n, int i) {
+        int v = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[v]) {
+            v = l;
+        }
+
+        if (r < n && arr[r] > arr[v]) {
+            v = r;
+        }
+
+        if (v != i) {
+            swap(arr, i, v);
+            /**
+             * recursion))))
+             */
+            sift(arr, n, v);
+        }
+    }
+}
---
+++ Task_1_1_1/src/test/java/org/nsu/syspro/Tests.java
@@ -0,0 +1,114 @@
+package org.nsu.syspro;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Random;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * class for Tests
+ */
+public class Tests {
+
+    /**
+     * Correctness testing
+     *
+     * @param arr
+     */
+    public static void Test(int[] arr) {
+        int[] copy = arr.clone();
+        HeapSort.sort(arr);
+        Arrays.sort(copy);
+        assertArrayEquals(arr, copy);
+    }
+
+    /**
+     * generate random array with len=len
+     * @param len
+     * @return
+     */
+    int[] RandomArray(int len) {
+        Random random = new Random();
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = random.nextInt();
+        }
+        return arr;
+    }
+
+    /**
+     * st tests
+     */
+
+    @Test
+    void testHeapSort() {
+        int[] arr = new int[]{4, 7, 8, 10, 3, 7, 10};
+        Test(arr);
+    }
+
+    @Test
+    void testEmptyArray() {
+        int[] arr = new int[]{};
+        Test(arr);
+    }
+
+    @Test
+    void testSingleElement() {
+        int[] arr = new int[]{42};
+        Test(arr);
+    }
+
+    @Test
+    void testSortedArray() {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        Test(arr);
+    }
+
+    @Test
+    void testReverseSortedArray() {
+        int[] arr = new int[]{5, 4, 3, 2, 1};
+        Test(arr);
+    }
+
+    @Test
+    void testArrayWithDuplicates() {
+        int[] arr = new int[]{5, 1, 5, 3, 1, 5, 2};
+        Test(arr);
+    }
+
+    @Test
+    void testAllSameElements() {
+        int[] arr = new int[]{7, 7, 7, 7, 7};
+        Test(arr);
+    }
+
+    @Test
+    void testArrayWithNegativeNumbers() {
+        int[] arr = new int[]{-3, -1, -7, 0, 2, 5, -2};
+        Test(arr);
+    }
+
+    @Test
+    void testArrayWithMinMaxIntegers() {
+        int[] arr = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0, -1, 1};
+        Test(arr);
+    }
+
+    /**
+     * test height
+     *     *           *         *
+     *    / \   and   /    and
+     *   *   *       *
+     *
+     */
+    @Test
+    void testAnySize() {
+        int h = 4;
+        for (int i = (int) Math.pow(2, (h - 1)); i < (int) Math.pow(2, h); i++) {
+            int[] arr = RandomArray(i);
+            Test(arr);
+        }
+    }
+}
