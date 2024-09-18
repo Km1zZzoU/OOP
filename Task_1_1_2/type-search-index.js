1a4cc613839f1c9f2a89a3a58dc7d8904d6296af
Km1zZzoU <Km1zZzoU@users.noreply.github.com>
2024-09-18 06:47:55 +0000

    Deploying to gh-pages from @ Km1zZzoU/OOP@07161f80d04649d3293b8ae23abe8058bc5ccd39 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.nsu","l":"Card"},{"p":"org.nsu","l":"Cards"},{"p":"org.nsu","l":"Deck"},{"p":"org.nsu","l":"Game"},{"p":"org.nsu","l":"Round"},{"p":"org.nsu","l":"Utils"}]
